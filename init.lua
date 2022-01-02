0aa751158cb755c4b81551847154a1a6095729b7
annnenpolka <k.miyakewiss1@gmail.com>
2022-01-02 16:40:36 +0900

    add impatient.nvim, Startup optimizer

--- init.lua
+++ init.lua
@@ -12,107 +12,119 @@ vim.cmd([[
   augroup end
 ]])
 
+require("impatient") -- optimize loading
+
 local use = require("packer").use
-require("packer").startup(function()
-	use("wbthomason/packer.nvim") -- Package manager
-	use("tpope/vim-fugitive") -- Git commands in nvim
-	use("tpope/vim-rhubarb") -- Fugitive-companion to interact with github
-	use("ludovicchabant/vim-gutentags") -- Automatic tags management
-	-- UI to select things (files, grep results, open buffers...)
-	use({ "nvim-telescope/telescope.nvim", requires = { "nvim-lua/plenary.nvim" } })
-	use("joshdick/onedark.vim") -- Theme inspired by Atom
-	use("itchyny/lightline.vim") -- Fancier statusline
-	-- Add indentation guides even on blank lines
-	use("lukas-reineke/indent-blankline.nvim")
-	-- Add git related info in the signs columns and popups
-	use({ "lewis6991/gitsigns.nvim", requires = { "nvim-lua/plenary.nvim" } })
-	-- Highlight, edit, and navigate code using a fast incremental parsing library
-	use("nvim-treesitter/nvim-treesitter")
-	-- Additional textobjects for treesitter
-	use({ "nvim-treesitter/nvim-treesitter-textobjects", requires = { "nvim-treesitter/nvim-treesitter" } })
-	use({
-		"neovim/nvim-lspconfig", -- Collection of configurations for built-in LSP client
-		"williamboman/nvim-lsp-installer", --  To Install LSP Automatically with LspInstall
-	})
-	use("hrsh7th/nvim-cmp") -- Autocompletion plugin
-	use("hrsh7th/cmp-nvim-lsp")
-	use("saadparwaiz1/cmp_luasnip")
-	use({ "tzachar/cmp-tabnine", run = "./install.sh", requires = "hrsh7th/nvim-cmp" })
-	-- Inject Format, Diagnostics, Code Actions to Lsp
-	use({ "jose-elias-alvarez/null-ls.nvim", requires = { "nvim-lua/plenary.nvim" } })
-	use("L3MON4D3/LuaSnip") -- Snippets plugin
-	use("rcarriga/nvim-notify") -- Notification baloon
-	-- Magit-like git plugin
-	use({
-		"TimUntersberger/neogit",
-		requires = {
-			"nvim-lua/plenary.nvim",
-			"sindrets/diffview.nvim",
+require("packer").startup({
+	function()
+		use("wbthomason/packer.nvim") -- Package manager
+		use("lewis6991/impatient.nvim") -- Optimize Lua modules loading
+		use("tpope/vim-fugitive") -- Git commands in nvim
+		use("tpope/vim-rhubarb") -- Fugitive-companion to interact with github
+		use("ludovicchabant/vim-gutentags") -- Automatic tags management
+		-- UI to select things (files, grep results, open buffers...)
+		use({ "nvim-telescope/telescope.nvim", requires = { "nvim-lua/plenary.nvim" } })
+		use("joshdick/onedark.vim") -- Theme inspired by Atom
+		use("itchyny/lightline.vim") -- Fancier statusline
+		-- Add indentation guides even on blank lines
+		use("lukas-reineke/indent-blankline.nvim")
+		-- Add git related info in the signs columns and popups
+		use({ "lewis6991/gitsigns.nvim", requires = { "nvim-lua/plenary.nvim" } })
+		-- Highlight, edit, and navigate code using a fast incremental parsing library
+		use("nvim-treesitter/nvim-treesitter")
+		-- Additional textobjects for treesitter
+		use({ "nvim-treesitter/nvim-treesitter-textobjects", requires = { "nvim-treesitter/nvim-treesitter" } })
+		use({
+			"neovim/nvim-lspconfig", -- Collection of configurations for built-in LSP client
+			"williamboman/nvim-lsp-installer", --  To Install LSP Automatically with LspInstall
+		})
+		use("hrsh7th/nvim-cmp") -- Autocompletion plugin
+		use("hrsh7th/cmp-nvim-lsp")
+		use("saadparwaiz1/cmp_luasnip")
+		use({ "tzachar/cmp-tabnine", run = "./install.sh", requires = "hrsh7th/nvim-cmp" })
+		-- Inject Format, Diagnostics, Code Actions to Lsp
+		use({ "jose-elias-alvarez/null-ls.nvim", requires = { "nvim-lua/plenary.nvim" } })
+		use("L3MON4D3/LuaSnip") -- Snippets plugin
+		use("rcarriga/nvim-notify") -- Notification baloon
+		-- Magit-like git plugin
+		use({
+			"TimUntersberger/neogit",
+			requires = {
+				"nvim-lua/plenary.nvim",
+				"sindrets/diffview.nvim",
+			},
+		})
+		use({ "sindrets/diffview.nvim", requires = { "kyazdani42/nvim-web-devicons" } }) -- diff viewer
+		use("ggandor/lightspeed.nvim") -- Faster f/F
+		use({
+			"yuki-yano/fuzzy-motion.vim",
+			requires = {
+				"vim-denops/denops.vim",
+			},
+		})
+		-- Surrounding plugin
+		use({
+			"blackCauldron7/surround.nvim",
+			config = function()
+				require("surround").setup({ mappings_style = "sandwich" })
+			end,
+		})
+		use("b3nj5m1n/kommentary") -- Commenting
+		use({ "p00f/nvim-ts-rainbow", requires = { "nvim-treesitter/nvim-treesitter" } }) -- Colorize brackets
+		use("simrat39/symbols-outline.nvim") -- outline viewer
+		use("kosayoda/nvim-lightbulb") -- Discover available code action like VSCode
+		use({
+			"weilbith/nvim-code-action-menu",
+			cmd = "CodeActionMenu",
+		})
+		use("kevinhwang91/nvim-hlslens") -- Show matched information in search
+		-- Show keybindings which-key
+		use({
+			"folke/which-key.nvim", -- Show keybindings
+			config = function()
+				require("which-key").setup({
+					-- your configuration comes here
+				})
+			end,
+		})
+		use("sainnhe/sonokai") -- sonokai theme
+		use("projekt0n/github-nvim-theme") -- github theme
+		-- Problem view
+		use({
+			"folke/trouble.nvim",
+			requires = "kyazdani42/nvim-web-devicons",
+			config = function()
+				require("trouble").setup({
+					-- your configuration comes here
+				})
+			end,
+		})
+		-- show signiture guides on type
+		use("ray-x/lsp_signature.nvim")
+		-- Open lazygit in neovim
+		use("kdheepak/lazygit.nvim")
+		-- autosave
+		use("Pocco81/AutoSave.nvim")
+		-- Autopair
+		use({
+			"windwp/nvim-autopairs",
+			config = function()
+				require("nvim-autopairs").setup({
+					check_ts = true,
+				})
+			end,
+		})
+		-- code biscuits
+		use({ "code-biscuits/nvim-biscuits", requires = { "nvim-treesitter/nvim-treesitter" } })
+	end,
+	-- Packer configuration
+	config = {
+		compile_path = vim.fn.stdpath("config") .. "/lua/packer_compiled.lua",
+		display = {
+			open_fn = require("packer.util").float,
 		},
-	})
-	use({ "sindrets/diffview.nvim", requires = { "kyazdani42/nvim-web-devicons" } }) -- diff viewer
-	use("ggandor/lightspeed.nvim") -- Faster f/F
-	use({
-		"yuki-yano/fuzzy-motion.vim",
-		requires = {
-			"vim-denops/denops.vim",
-		},
-	})
-	-- Surrounding plugin
-	use({
-		"blackCauldron7/surround.nvim",
-		config = function()
-			require("surround").setup({ mappings_style = "sandwich" })
-		end,
-	})
-	use("b3nj5m1n/kommentary") -- Commenting
-	use({ "p00f/nvim-ts-rainbow", requires = { "nvim-treesitter/nvim-treesitter" } }) -- Colorize brackets
-	use("simrat39/symbols-outline.nvim") -- outline viewer
-	use("kosayoda/nvim-lightbulb") -- Discover available code action like VSCode
-	use({
-		"weilbith/nvim-code-action-menu",
-		cmd = "CodeActionMenu",
-	})
-	use("kevinhwang91/nvim-hlslens") -- Show matched information in search
-	-- Show keybindings which-key
-	use({
-		"folke/which-key.nvim", -- Show keybindings
-		config = function()
-			require("which-key").setup({
-				-- your configuration comes here
-			})
-		end,
-	})
-	use("sainnhe/sonokai") -- sonokai theme
-	use("projekt0n/github-nvim-theme") -- github theme
-	-- Problem view
-	use({
-		"folke/trouble.nvim",
-		requires = "kyazdani42/nvim-web-devicons",
-		config = function()
-			require("trouble").setup({
-				-- your configuration comes here
-			})
-		end,
-	})
-	-- show signiture guides on type
-	use("ray-x/lsp_signature.nvim")
-	-- Open lazygit in neovim
-	use("kdheepak/lazygit.nvim")
-	-- autosave
-	use("Pocco81/AutoSave.nvim")
-	-- Autopair
-	use({
-		"windwp/nvim-autopairs",
-		config = function()
-			require("nvim-autopairs").setup({
-				check_ts = true,
-			})
-		end,
-	})
-	-- code biscuits
-	use({ "code-biscuits/nvim-biscuits", requires = { "nvim-treesitter/nvim-treesitter" } })
-end)
+	},
+})
 
 -- autoread
 vim.g.autoread = true
