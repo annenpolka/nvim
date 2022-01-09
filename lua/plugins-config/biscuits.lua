707ab6a50fc5d5a4d7a69c7a979b8078faba77c3
annnenpolka <k.miyakewiss1@gmail.com>
2022-01-09 21:54:29 +0900

    chore: biscuits config only cursor enabled

--- lua/plugins-config/biscuits.lua
+++ lua/plugins-config/biscuits.lua
@@ -1,22 +1,22 @@
 local M = {}
 
 function M.config()
-    -- Code Biscuits
-    local biscuits = require("nvim-biscuits").setup({
-    	toggle_keybind = "<leader>bs",
-    	show_on_start = true,
-    	cursor_line_only = false,
-    	default_config = {
-    		max_length = 120,
-    		min_distance = 3,
-    		prefix_string = "<- ",
-    	},
-    	language_config = {
-    		python = {
-    			disabled = true,
-    		},
-    	},
-    })
+	-- Code Biscuits
+	local biscuits = require("nvim-biscuits").setup({
+		toggle_keybind = "<leader>bs",
+		cursor_line_only = true,
+		show_on_start = true,
+		default_config = {
+			max_length = 120,
+			min_distance = 3,
+			prefix_string = "<- ",
+		},
+		language_config = {
+			python = {
+				disabled = true,
+			},
+		},
+	})
 end
 
 return M
