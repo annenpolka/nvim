0ef0af6fca524c03ee46ebb4be07d416e48f3026
annnenpolka <k.miyakewiss1@gmail.com>
2022-01-11 21:37:10 +0900

    set copilot disable by default

--- lua/plugins-config/copilot.lua
+++ lua/plugins-config/copilot.lua
@@ -1,8 +1,11 @@
 local M = {}
 
 function M.config()
-	vim.cmd([[imap <silent><script><expr> <C-J> copilot#Accept("\<CR>")
-            let g:copilot_no_tab_map = v:true]])
+	vim.cmd([[
+            imap <silent><script><expr> <C-J> copilot#Accept("\<CR>")
+            let g:copilot_no_tab_map = v:true
+            let b:copilot_enabled = v:false
+  ]])
 end
 
 return M
