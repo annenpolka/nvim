d8edd3e2b727c79dafdc729e5985554e409166fc
annnenpolka <k.miyakewiss1@gmail.com>
2022-01-13 23:08:17 +0900

    adjust focus keybindings, add excluded_filetypes

--- lua/plugins-config/focus.lua
+++ lua/plugins-config/focus.lua
@@ -1,14 +1,15 @@
 local M = {}
 
 function M.config()
-	require("focus").setup()
+	require("focus").setup({ excluded_filetypes = { "toggleterm", "harpoon" } })
 end
 
 function M.map()
-    vim.api.nvim_set_keymap("n", "<leader>h", ":FocusSplitLeft<CR>", { silent = true })
-    vim.api.nvim_set_keymap("n", "<leader>j", ":FocusSplitDown<CR>", { silent = true })
-    vim.api.nvim_set_keymap("n", "<leader>k", ":FocusSplitUp<CR>", { silent = true })
-    vim.api.nvim_set_keymap("n", "<leader>l", ":FocusSplitRight<CR>", { silent = true })
+	vim.api.nvim_set_keymap("n", "<leader>h", ":FocusSplitLeft<CR>", { silent = true })
+	vim.api.nvim_set_keymap("n", "<leader>j", ":FocusSplitDown<CR>", { silent = true })
+	vim.api.nvim_set_keymap("n", "<leader>k", ":FocusSplitUp<CR>", { silent = true })
+	vim.api.nvim_set_keymap("n", "<leader>l", ":FocusSplitRight<CR>", { silent = true })
+	vim.api.nvim_set_keymap("n", "<CR>", ":FocusMaximise<CR>", { silent = true })
 end
 
 return M
