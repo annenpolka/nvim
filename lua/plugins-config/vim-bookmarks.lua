5e946f5d24d3367bb47c3b86ffdbc8a32b1115f5
annnenpolka <k.miyakewiss1@gmail.com>
2022-01-16 13:01:45 +0900

    remap bookmark_next/prev to <leader><C-i>/<C-o>, display annotation

--- lua/plugins-config/vim-bookmarks.lua
+++ lua/plugins-config/vim-bookmarks.lua
@@ -5,14 +5,15 @@ function M.config()
 	vim.g.bookmark_save_per_working_dir = 1
 	vim.g.bookmark_auto_save = 1
 	vim.g.bookmark_center = 1
+	vim.g.bookmark_display_annotation = 1
 end
 
 function M.map()
 	vim.api.nvim_set_keymap("n", "<C-m>", "<Plug>BookmarkToggle", { silent = true })
 	vim.api.nvim_set_keymap("n", "<C-a>", "<Plug>BookmarkAnnotate", { silent = true })
 	vim.api.nvim_set_keymap("n", "<leader>bb", "<Plug>BookmarkShowAll", { silent = true })
-	vim.api.nvim_set_keymap("n", "<leader>bj", "<Plug>BookmarkNext", { silent = true })
-	vim.api.nvim_set_keymap("n", "<leader>bk", "<Plug>BookmarkPrev", { silent = true })
+	vim.api.nvim_set_keymap("n", "<leader><C-i>", "<Plug>BookmarkNext", { silent = true })
+	vim.api.nvim_set_keymap("n", "<leader><C-a>", "<Plug>BookmarkPrev", { silent = true })
 	vim.api.nvim_set_keymap("n", "<leader>bc", "<Plug>BookmarkClear", { silent = true })
 	vim.api.nvim_set_keymap("n", "<leader>bx", "<Plug>BookmarkClearAll", { silent = true })
 end
