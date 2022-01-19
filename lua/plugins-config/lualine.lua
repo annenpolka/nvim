eab93ddb99f17d7dae9e20a2d3bdaf8bd781a447
annnenpolka <k.miyakewiss1@gmail.com>
2022-01-19 14:06:29 +0900

    chore: remove lualine's tab info

--- lua/plugins-config/lualine.lua
+++ lua/plugins-config/lualine.lua
@@ -13,7 +13,7 @@ function M.config()
 		sections = {
 			lualine_a = { "mode" },
 			lualine_b = { "branch", "diff", "diagnostics" },
-			lualine_c = { "tabs", "filename" },
+			lualine_c = { "filename" },
 			lualine_x = { "encoding", "fileformat", "filetype" },
 			lualine_y = { "progress" },
 			lualine_z = { "location" },
