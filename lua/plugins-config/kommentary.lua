95f3e234db5ec879978e21185b76770a92f92a3d
annnenpolka <k.miyakewiss1@gmail.com>
2022-01-22 21:39:40 +0900

    add kommentary confitguration, prefer single-line comments

--- lua/plugins-config/kommentary.lua
+++ lua/plugins-config/kommentary.lua
@@ -1,14 +1,20 @@
 local M = {}
 
+function M.config()
+	require("kommentary.config").configure_language("default", {
+		prefer_single_line_comments = true,
+	})
+end
+
 function M.map()
-    vim.api.nvim_set_keymap("n", "<leader>cic", "<Plug>kommentary_line_increase", {})
-    vim.api.nvim_set_keymap("n", "<leader>ci", "<Plug>kommentary_motion_increase", {})
-    vim.api.nvim_set_keymap("x", "<leader>ci", "<Plug>kommentary_visual_increase", {})
-    vim.api.nvim_set_keymap("n", "<leader>cdc", "<Plug>kommentary_line_decrease", {})
-    vim.api.nvim_set_keymap("n", "<leader>cd", "<Plug>kommentary_motion_decrease", {})
-    vim.api.nvim_set_keymap("x", "<leader>cd", "<Plug>kommentary_visual_decrease", {})
-    vim.api.nvim_set_keymap("n", "<C-_>", "<Plug>kommentary_line_default", {})
-    vim.api.nvim_set_keymap("v", "<C-_>", "<Plug>kommentary_visual_default<C-c>", {})
+	vim.api.nvim_set_keymap("n", "<leader>cic", "<Plug>kommentary_line_increase", {})
+	vim.api.nvim_set_keymap("n", "<leader>ci", "<Plug>kommentary_motion_increase", {})
+	vim.api.nvim_set_keymap("x", "<leader>ci", "<Plug>kommentary_visual_increase", {})
+	vim.api.nvim_set_keymap("n", "<leader>cdc", "<Plug>kommentary_line_decrease", {})
+	vim.api.nvim_set_keymap("n", "<leader>cd", "<Plug>kommentary_motion_decrease", {})
+	vim.api.nvim_set_keymap("x", "<leader>cd", "<Plug>kommentary_visual_decrease", {})
+	vim.api.nvim_set_keymap("n", "<C-_>", "<Plug>kommentary_line_default", {})
+	vim.api.nvim_set_keymap("v", "<C-_>", "<Plug>kommentary_visual_default<C-c>", {})
 end
 
 return M
