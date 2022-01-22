1e4334ad2f579a3017632d3a6c794ff928590788
annnenpolka <k.miyakewiss1@gmail.com>
2022-01-22 21:33:37 +0900

    add treesitter-context configuration

---
+++ lua/plugins-config/treesitter-context.lua
@@ -0,0 +1,33 @@
+local M = {}
+
+function M.config()
+	require("treesitter-context").setup({
+		enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+		throttle = true, -- Throttles plugin updates (may improve performance)
+		max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+		patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+			-- For all filetypes
+			-- Note that setting an entry here replaces all other patterns for this entry.
+			-- By setting the 'default' entry below, you can control which nodes you want to
+			-- appear in the context window.
+			default = {
+				"class",
+				"function",
+				"method",
+				"for", -- These won't appear in the context
+				"while",
+				"if",
+				"switch",
+				"case",
+			},
+		},
+		exact_patterns = {
+			-- Example for a specific filetype with Lua patterns
+			-- Treat patterns.rust as a Lua pattern (i.e "^impl_item$" will
+			-- exactly match "impl_item" only)
+			-- rust = true,
+		},
+	})
+end
+
+return M
