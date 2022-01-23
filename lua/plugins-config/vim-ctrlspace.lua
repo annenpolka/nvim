eb1e9c602f7a90905d18d825c796950a1fcd9db5
annnenpolka <k.miyakewiss1@gmail.com>
2022-01-23 16:51:13 +0900

    changed ctrlspace settigs, bascially doesn't save workspace

--- lua/plugins-config/vim-ctrlspace.lua
+++ lua/plugins-config/vim-ctrlspace.lua
@@ -1,15 +1,16 @@
 local M = {}
 
 function M.config()
-	vim.g.CtrlSpaceDefaultMappingKey = "<C-a> "
+	vim.g.CtrlSpaceDefaultMappingKey = "<leader>z "
 	vim.g.CtrlSpaceUseTabline = 0
 	vim.g.CtrlSpaceSearchTiming = 50
-	vim.g.CtrlSpaceSaveWorkspaceOnExit = 1
+	vim.g.CtrlSpaceSaveWorkspaceOnExit = 0
 	vim.g.CtrlSpaceLoadLastWorkspaceOnStart = 0
-	vim.g.CtrlSpaceSaveWorkspaceOnSwitch = 1
+	vim.g.CtrlSpaceSaveWorkspaceOnSwitch = 0
 end
 
 function M.map()
+	vim.api.nvim_set_keymap("n", "<C-a>", ":CtrlSpace a<CR>", { noremap = true, silent = true })
 	vim.api.nvim_set_keymap("n", "-", ":CtrlSpaceGoDown<CR>", { noremap = true, silent = true })
 	vim.api.nvim_set_keymap("n", "^", ":CtrlSpaceGoUp<CR>", { noremap = true, silent = true })
 end
