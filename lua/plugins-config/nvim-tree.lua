b196a12d5e8f00ec210aa1c01e8de67af7f8ddcd
annnenpolka <k.miyakewiss1@gmail.com>
2022-01-24 13:32:04 +0900

    open nvim-tree on startup

--- lua/plugins-config/nvim-tree.lua
+++ lua/plugins-config/nvim-tree.lua
@@ -7,7 +7,7 @@ function M.config()
 	local nvimtree_config = {
 		disable_netrw = true,
 		hijack_netrw = true,
-		open_on_setup = false,
+		open_on_setup = true,
 		ignore_ft_on_setup = {},
 		auto_close = false,
 		open_on_tab = false,
