3181d0087968b880f9914d92b522cb501a19d207
annnenpolka <k.miyakewiss1@gmail.com>
2022-01-27 22:52:29 +0900

    set Illuminate_delay(word highlighting delay), new colors

--- lua/plugins-config/vim-illuminate.lua
+++ lua/plugins-config/vim-illuminate.lua
@@ -1,10 +1,11 @@
 local M = {}
 
 function M.config()
+	vim.g.Illuminate_delay = 300
 	vim.cmd([[
 		augroup illuminate_augroup
 			autocmd!
-			autocmd VimEnter * hi illuminatedWord cterm=underline gui=underline guibg=NONE
+			autocmd VimEnter * hi illuminatedWord cterm=underline gui=underline guibg=#16162D
 			autocmd VimEnter * hi illuminatedCurWord guibg=#16162D
 		augroup END
 	]])
