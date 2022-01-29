4cb730146d9f9e8154215041b2ba8928bc5a9950
annnenpolka <k.miyakewiss1@gmail.com>
2022-01-29 19:47:46 +0900

    chore: fix mapping

--- lua/plugins-config/neogit.lua
+++ lua/plugins-config/neogit.lua
@@ -1,20 +1,20 @@
 local M = {}
 
 function M.config()
-    -- Neogit Settings
-    -- -- setup
-    local neogit = require("neogit")
-    neogit.setup({
-	disable_commit_confirmation = true,
-    	integrations = {
-    		diffview = true, -- set diffview.nvim integration
-    	},
-    })
+	-- Neogit Settings
+	-- -- setup
+	local neogit = require("neogit")
+	neogit.setup({
+		disable_commit_confirmation = true,
+		integrations = {
+			diffview = true, -- set diffview.nvim integration
+		},
+	})
 end
 
 function M.map()
-    vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>Neogit<CR>", { noremap = true })
-    nnoremap("<C-c><C-c>", ":wq<CR>", { ft = "NeogitCommitMessages" } )
+	vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>Neogit<CR>", { noremap = true })
+	nnoremap("<C-c><C-c>", ":wq<CR>", { ft = "NeogitCommitMessage" })
 end
 
 return M
