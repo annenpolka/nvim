0289ea9d96321e623c0e0436b238f01c3948b4fd
annnenpolka <k.miyakewiss1@gmail.com>
2022-01-30 10:05:50 +0900

    set treehopper hint keys

--- lua/plugins-config/nvim-treehopper.lua
+++ lua/plugins-config/nvim-treehopper.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 function M.map()
+	require("tsht").config.hint_keys = { "h", "j", "f", "d", "n", "v", "s", "l", "a" }
 	omap("m", ":<C-U>lua require('tsht').nodes()<CR>", { silent = true })
 	vnoremap("m", ":lua require('tsht').nodes()<CR>", { silent = true })
 end
