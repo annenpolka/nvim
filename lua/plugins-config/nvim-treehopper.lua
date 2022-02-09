280f604aa720b506f04a12facba34d0df52d4d92
Annenpolka <lancelbb@gmail.com>
2022-02-09 18:50:42 +0900

    move nvim-treehopper require to config()

--- lua/plugins-config/nvim-treehopper.lua
+++ lua/plugins-config/nvim-treehopper.lua
@@ -1,7 +1,9 @@
 local M = {}
 
-function M.map()
+function M.config()
 	require("tsht").config.hint_keys = { "h", "j", "f", "d", "n", "v", "s", "l", "a" }
+end
+function M.map()
 	omap("m", ":<C-U>lua require('tsht').nodes()<CR>", { silent = true })
 	vnoremap("m", ":lua require('tsht').nodes()<CR>", { silent = true })
 end
