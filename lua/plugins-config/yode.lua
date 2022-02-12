faf2c95dbbd0758db0561ea245073c5067dc8c51
annnenpolka <k.miyakewiss1@gmail.com>
2022-02-12 19:14:18 +0900

    add yode mappings

--- lua/plugins-config/yode.lua
+++ lua/plugins-config/yode.lua
@@ -4,4 +4,10 @@ function M.config()
 	require("yode-nvim").setup()
 end
 
+function M.map()
+	nmap("<C-y>", "vai:YodeCreateSeditorFloating<CR>")
+	xmap("<C-y>", ":YodeCreateSeditorFloating<CR>")
+	xmap("<leader>yr", ":YodeCreateSeditorReplace<CR>")
+end
+
 return M
