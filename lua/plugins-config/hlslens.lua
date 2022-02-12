a5515028c7f9affcdb1cae5f300750e87d1f334f
annnenpolka <k.miyakewiss1@gmail.com>
2022-02-12 19:34:05 +0900

    centerize n/N search cursor

--- lua/plugins-config/hlslens.lua
+++ lua/plugins-config/hlslens.lua
@@ -33,12 +33,42 @@ function M.config()
 end
 
 function M.map()
-	vim.api.nvim_set_keymap("n", "n", [[n<Cmd>lua require('hlslens').start()<CR>]], { noremap = true, silent = true })
-	vim.api.nvim_set_keymap("n", "N", [[N<Cmd>lua require('hlslens').start()<CR>]], { noremap = true, silent = true })
-	vim.api.nvim_set_keymap("n", "*", [[*<Cmd>lua require('hlslens').start()<CR>]], { noremap = true, silent = true })
-	vim.api.nvim_set_keymap("n", "#", [[#<Cmd>lua require('hlslens').start()<CR>]], { noremap = true, silent = true })
-	vim.api.nvim_set_keymap("n", "g*", [[g*<Cmd>lua require('hlslens').start()<CR>]], { noremap = true, silent = true })
-	vim.api.nvim_set_keymap("n", "g#", [[g#<Cmd>lua require('hlslens').start()<CR>]], { noremap = true, silent = true })
+	vim.api.nvim_set_keymap(
+		"n",
+		"n",
+		[[n<Cmd>lua require('hlslens').start()<CR>zzzv]],
+		{ noremap = true, silent = true }
+	)
+	vim.api.nvim_set_keymap(
+		"n",
+		"N",
+		[[N<Cmd>lua require('hlslens').start()<CR>zzzv]],
+		{ noremap = true, silent = true }
+	)
+	vim.api.nvim_set_keymap(
+		"n",
+		"*",
+		[[*<Cmd>lua require('hlslens').start()<CR>zzzv]],
+		{ noremap = true, silent = true }
+	)
+	vim.api.nvim_set_keymap(
+		"n",
+		"#",
+		[[#<Cmd>lua require('hlslens').start()<CR>zzzv]],
+		{ noremap = true, silent = true }
+	)
+	vim.api.nvim_set_keymap(
+		"n",
+		"g*",
+		[[g*<Cmd>lua require('hlslens').start()<CR>zzzv]],
+		{ noremap = true, silent = true }
+	)
+	vim.api.nvim_set_keymap(
+		"n",
+		"g#",
+		[[g#<Cmd>lua require('hlslens').start()<CR>zzzv]],
+		{ noremap = true, silent = true }
+	)
 	vim.api.nvim_set_keymap("n", "<leader>L", ":nohl<CR>", { noremap = true, silent = true })
 end
 
