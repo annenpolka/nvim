8d0bbefd57008f77f2e1fec915187b6fc9725598
annnenpolka <k.miyakewiss1@gmail.com>
2022-03-05 15:44:39 +0900

    lazyload save point (decided to remove them after now)

--- lua/plugins-config/init.lua
+++ lua/plugins-config/init.lua
@@ -8,7 +8,7 @@ vim.cmd("packadd vim-jetpack")
 require("jetpack").startup(function(use)
 	use({
 		"tani/vim-jetpack",
-		opt = 1,
+		-- opt = 1,
 	})
 	-- lua loading optimizer
 	use({ "lewis6991/impatient.nvim" })
@@ -44,7 +44,7 @@ require("jetpack").startup(function(use)
 	-- sudo
 	use({
 		"lambdalisue/suda.vim",
-		opt = 1,
+		-- -- opt = 1,
 		on = { "SudaWrite", "SudaRead" },
 	})
 	-- Session manager
@@ -60,7 +60,7 @@ require("jetpack").startup(function(use)
 	-- Better Escape
 	use({
 		"max397574/better-escape.nvim",
-		opt = 1,
+		-- opt = 1,
 		-- on = "InsertEnter",
 	})
 	-- Better line number jump with peek
@@ -77,13 +77,13 @@ require("jetpack").startup(function(use)
 	-- readline keybindings in insert mode
 	use({
 		"tpope/vim-rsi",
-		opt = 1,
+		-- opt = 1,
 		-- on = "InsertEnter",
 	})
 	-- switch working directory
 	use({
 		"nyngwang/NeoRoot.lua",
-		opt = 1,
+		-- opt = 1,
 		on = {
 			"NeoRoot",
 			"NeoRootChange",
@@ -110,7 +110,7 @@ require("jetpack").startup(function(use)
 	-- Add git related info in the signs columns and popups
 	use({
 		"lewis6991/gitsigns.nvim",
-		opt = 1,
+		-- opt = 1,
 	})
 	-- Github editor
 	use({
@@ -123,7 +123,7 @@ require("jetpack").startup(function(use)
 	-- search buffer
 	use({
 		"nvim-pack/nvim-spectre",
-		opt = 1,
+		-- opt = 1,
 		-- on = "BufWinEnter",
 	})
 	-- Automatic tags management
@@ -144,7 +144,7 @@ require("jetpack").startup(function(use)
 	-- use{ "kyazdani42/nvim-tree.lua" },
 	use({
 		"elihunter173/dirbuf.nvim",
-		opt = 1,
+		-- opt = 1,
 		on = "Dirbuf",
 	})
 	-- ╭──────────────────────────────────────────────────────────╮
@@ -242,18 +242,18 @@ require("jetpack").startup(function(use)
 	-- Fancier statusline
 	use({
 		"nvim-lualine/lualine.nvim",
-		opt = 1,
+		-- opt = 1,
 		-- on = "BufRead",
 	})
 	-- scrollbar with search info
 	use({
 		"petertriho/nvim-scrollbar",
-		opt = 1,
+		-- opt = 1,
 	})
 	-- Add indentation guides even on blank lines
 	use({
 		"lukas-reineke/indent-blankline.nvim",
-		opt = 1,
+		-- opt = 1,
 		-- on = "BufRead",
 	})
 	use({ "echasnovski/mini.nvim" })
@@ -263,7 +263,7 @@ require("jetpack").startup(function(use)
 	use({
 		"nvim-treesitter/nvim-treesitter",
 		run = ":TSUpdate",
-		opt = 1,
+		-- opt = 1,
 	})
 	-- playground
 	use({ "nvim-treesitter/playground" })
@@ -300,7 +300,7 @@ require("jetpack").startup(function(use)
 	-- show signature guides on type
 	use({
 		"ray-x/lsp_signature.nvim",
-		opt = 1,
+		-- opt = 1,
 	})
 	-- code outline
 	use({ "stevearc/aerial.nvim" })
@@ -312,8 +312,7 @@ require("jetpack").startup(function(use)
 	-- ╰──────────────────────────────────────────────────────────╯
 	use({
 		"hrsh7th/nvim-cmp",
-		-- on = { "InsertEnter", "CmdlineEnter" },
-		opt = 1,
+		-- opt = 1,
 	})
 	-- -- cmp devicons appearance dependency
 	use({
@@ -354,7 +353,7 @@ require("jetpack").startup(function(use)
 	-- github copilot
 	use({
 		"github/copilot.vim",
-		opt = 1,
+		-- opt = 1,
 	})
 	use({
 		"hrsh7th/cmp-copilot", -- after = "nvim-cmp",
@@ -375,22 +374,22 @@ require("jetpack").startup(function(use)
 	-- Debug Adapter Protocol
 	use({
 		"mfussenegger/nvim-dap",
-		opt = 1,
+		-- opt = 1,
 	})
 	-- DAP UI
 	use({
 		"rcarriga/nvim-dap-ui", -- after = "nvim-dap",
-		opt = 1,
+		-- opt = 1,
 	})
 	-- vitrual text
 	use({
 		"theHamsta/nvim-dap-virtual-text", -- after = "nvim-dap",
-		opt = 1,
+		-- opt = 1,
 	})
 	-- DAP Installer
 	use({
 		"Pocco81/DAPInstall.nvim", -- after = "nvim-dap",
-		opt = 1,
+		-- opt = 1,
 	})
 	-- async task runners
 	use({ "skywind3000/asyncrun.vim" })
@@ -401,7 +400,10 @@ require("jetpack").startup(function(use)
 	-- │                           Misc                           │
 	-- ╰──────────────────────────────────────────────────────────╯
 	-- spellcheck
-	use({ "kamykn/spelunker.vim" })
+	use({
+		"kamykn/spelunker.vim",
+		-- opt = 1,
+	})
 	-- tabout
 	use({
 		"abecodes/tabout.nvim",
@@ -421,7 +423,7 @@ require("jetpack").startup(function(use)
 	use({
 		"phaazon/hop.nvim",
 		branch = "master",
-		opt = 1,
+		-- opt = 1,
 		on = {
 			"HopWord",
 			"HopWordBC",
@@ -470,7 +472,7 @@ require("jetpack").startup(function(use)
 	-- Show keybindings
 	use({
 		"folke/which-key.nvim",
-		opt = 1,
+		-- opt = 1,
 	})
 	-- Problem view
 	use({
@@ -480,7 +482,7 @@ require("jetpack").startup(function(use)
 	-- highlighting the word under cursor
 	use({
 		"RRethy/vim-illuminate",
-		opt = 1,
+		-- opt = 1,
 	})
 	-- Autopair
 	use({
@@ -505,7 +507,7 @@ require("jetpack").startup(function(use)
 	-- automatic split management
 	use({
 		"beauwilliams/focus.nvim",
-		opt = 1,
+		-- opt = 1,
 		-- on = { "FocusSplitDown", "FocusSplitUp", "FocusSplitLeft", "FocusSplitRight" },
 	})
 	-- Zen mode
@@ -561,10 +563,19 @@ end
 
 -- load config on jetpack autocmd
 -- name for vimscript is plugin's name, name for lua is its module name
-local function set_jetpack_event(name, config)
+local function load_config_on_jetpack(name, config)
 	local event = vim.fn.substitute(name, [[\W\+]], [[_]], "g")
 	event = vim.fn.substitute(event, [[\(^\|_\)\(.\)]], [[\u\2]], "g")
 	vim.cmd([[autocmd! User Jetpack]] .. event .. [[ lua require("plugins-config.]] .. config .. [[").config()]])
+end
+
+local function packadd_on_jetpack(name, plugin, module)
+	local event = vim.fn.substitute(name, [[\W\+]], [[_]], "g")
+	event = vim.fn.substitute(event, [[\(^\|_\)\(.\)]], [[\u\2]], "g")
+	vim.cmd(
+		[[autocmd! User Jetpack]] .. event .. [[ packadd ]] .. plugin -- .. [[ | lua pcall(require,"]] .. module .. [[")]]
+	)
+	-- print(module .. " loaded")
 end
 
 -- packadd timer
@@ -580,7 +591,7 @@ end
 
 -- lazyload by timer
 local function lazyload_timer(plugin, module, config, timer)
-	set_jetpack_event(module, config)
+	load_config_on_jetpack(module, config)
 	packadd_timer(plugin, module, timer)
 end
 
@@ -599,27 +610,91 @@ local function load_config()
 	require("plugins-config.matchup").config()
 	require("plugins-config.kommentary").config()
 
+	-- config list
+	vim.o.termguicolors = true
+	require("plugins-config.nvim-treesitter").config()
+	require("plugins-config.nvim-cmp").config()
+	require("plugins-config.aerial").config()
+	require("plugins-config.autopairs").config()
+	require("plugins-config.better-escape").config()
+	require("plugins-config.blamer").config()
+	require("plugins-config.colorizer").config()
+	require("plugins-config.context_vt").config()
+	require("plugins-config.copilot").config()
+	require("plugins-config.dap").config()
+	require("plugins-config.dirbuf").config()
+	require("plugins-config.dressing").config()
+	require("plugins-config.everforest").config()
+	require("plugins-config.focus").config()
+	require("plugins-config.gesture").config()
+	require("plugins-config.gitsigns").config()
+	require("plugins-config.harpoon").config()
+	require("plugins-config.hlslens").config()
+	require("plugins-config.hop").config()
+	require("plugins-config.indent-blankline").config()
+	require("plugins-config.kommentary").config()
+	require("plugins-config.lightbulb").config()
+	require("plugins-config.lsp_signature").config()
+	require("plugins-config.matchup").config()
+	require("plugins-config.mini-indentscope").config()
+	require("plugins-config.mini-starter").config()
+	require("plugins-config.neogen").config()
+	require("plugins-config.neogit").config()
+	require("plugins-config.neovim-session-manager").config()
+	require("plugins-config.notify").config()
+	require("plugins-config.numb").config()
+	require("plugins-config.nvim-gps").config()
+	require("plugins-config.nvim-treehopper").config()
+	require("plugins-config.octo").config()
+	require("plugins-config.pounce").config()
+	require("plugins-config.pretty-fold").config()
+	require("plugins-config.qf_helper").config()
+	require("plugins-config.registers").config()
+	require("plugins-config.sandwich").config()
+	require("plugins-config.scrollbar").config()
+	require("plugins-config.spectre").config()
+	require("plugins-config.spelunker").config()
+	require("plugins-config.substitute").config()
+	require("plugins-config.symbols-outline").config()
+	require("plugins-config.tabby").config()
+	require("plugins-config.tabout").config()
+	require("plugins-config.telescope").config()
+	require("plugins-config.todo-comments").config()
+	require("plugins-config.toggleterm").config()
+	require("plugins-config.treesitter-context").config()
+	require("plugins-config.treesitter-unit").config()
+	require("plugins-config.trouble").config()
+	require("plugins-config.twilight").config()
+	require("plugins-config.vim-bookmarks").config()
+	require("plugins-config.vim-ctrlspace").config()
+	require("plugins-config.vim-illuminate").config()
+	require("plugins-config.which-key").config()
+	require("plugins-config.yode").config()
+	require("plugins-config.zen-mode").config()
+	-- require("plugins-config.zenbones").config()
+
 	-- load config on require(lua), packadd(vimscript)
-	set_jetpack_event("dressing", "dressing")
-	set_jetpack_event("session-manager", "neovim-session-manager")
-	set_jetpack_event("gesture", "gesture")
-	set_jetpack_event("neogit", "neogit")
-	set_jetpack_event("hop", "hop")
-	set_jetpack_event("dirbuf", "dirbuf")
+	load_config_on_jetpack("dressing", "dressing")
+	load_config_on_jetpack("session-manager", "neovim-session-manager")
+	load_config_on_jetpack("gesture", "gesture")
+	load_config_on_jetpack("neogit", "neogit")
+	load_config_on_jetpack("hop", "hop")
+	load_config_on_jetpack("dirbuf", "dirbuf")
 
 	-- lazyload
 	-- -- timer
 	lazyload_timer("nvim-spectre", "spectre", "spectre", 1000)
 	lazyload_timer("lualine.nvim", "lualine", "lualine", 100)
 	-- -- event
-	load_on_event("BufWinEnter", "gitsigns.nvim", "gitsigns")
-	load_on_event("BufRead,BufNewFile", "nvim-scrollbar", "scrollbar")
-	load_on_event("BufRead,BufNewFile", "which-key.nvim", "which-key")
-	load_on_event("BufRead,BufNewFile", "yode-nvim", "yode")
-	load_on_event("BufRead,BufNewFile", "copilot.vim", "copilot")
-	load_on_event("BufWinEnter", "focus.nvim", "focus")
-	load_on_event("BufWinEnter", "indent-blankline.nvim", "indent-blankline")
-	load_on_event("BufWinEnter", "vim-illuminate", "vim-illuminate")
+	load_on_event("FileType", "gitsigns.nvim", "gitsigns")
+	load_on_event("FileType,BufRead,BufNewFile", "substitute.nvim", "substitute")
+	load_on_event("FileType,BufRead,BufNewFile", "nvim-scrollbar", "scrollbar")
+	load_on_event("FileType,BufRead,BufNewFile", "which-key.nvim", "which-key")
+	load_on_event("FileType,BufRead,BufNewFile", "yode-nvim", "yode")
+	load_on_event("FileType,BufRead,BufNewFile", "copilot.vim", "copilot")
+	load_on_event("FileType,BufRead,BufNewFile", "focus.nvim", "focus")
+	load_on_event("FileType,BufRead,BufNewFile", "indent-blankline.nvim", "indent-blankline")
+	load_on_event("FileType,BufRead,BufNewFile", "vim-illuminate", "vim-illuminate")
 	load_on_event("CmdlineEnter", "numb", "numb")
 	load_on_event("CmdlineEnter", "octo.nvim", "octo")
 	load_on_event("InsertEnter", "better-escape.nvim", "better-escape")
@@ -645,40 +720,54 @@ local function load_config()
   ]])
 
 	-- nvim-cmp things
-	load_on_event("InsertEnter", "nvim-cmp", "nvim-cmp")
-	load_on_event("CmdlineEnter", "nvim-cmp", "nvim-cmp")
+	load_on_event("InsertEnter,CmdlineEnter", "nvim-cmp", "nvim-cmp")
+	packadd_on_jetpack("nvim-cmp", "lspkind-nvim")
+	packadd_on_jetpack("nvim-cmp", "cmp-nvim-lsp")
+	packadd_on_jetpack("nvim-cmp", "cmp_luasnip")
+	packadd_on_jetpack("nvim-cmp", "cmp-path")
+	packadd_on_jetpack("nvim-cmp", "cmp-nvim-lsp-document-symbol")
+	packadd_on_jetpack("nvim-cmp", "cmp-buffer")
+	packadd_on_jetpack("nvim-cmp", "cmp-cmdline")
+	packadd_on_jetpack("nvim-cmp", "cmp-look")
+	packadd_on_jetpack("nvim-cmp", "cmp-treesitter")
+	packadd_on_jetpack("nvim-cmp", "cmp-rg")
+	packadd_on_jetpack("nvim-cmp", "cmp-under-comparator")
+	packadd_on_jetpack("nvim-cmp", "cmp-copilot")
+	packadd_on_jetpack("nvim-cmp", "friendly-snippets")
+	packadd_on_jetpack("nvim-cmp", "LuaSnip")
+
 	-- TODO: lazyload nvim-cmp related plugins
 
 	-- nvim-dap things
-	-- load_on_event("BufWinEnter", "nvim-dap")
-	-- load_on_event("BufWinEnter", "nvim-dap-ui")
-	-- load_on_event("BufWinEnter", "nvim-dap-virtual-text")
-	-- load_on_event("BufWinEnter", "DAPInstall.nvim", "dap-install")
-	-- vim.cmd([[
-	--    augroup DapRelated
-	--     autocmd!
-	--     autocmd User JetpackDapInstall lua require("plugins-config.dap").config()
-	--    augroup END
-	--  ]])
+	load_on_event("BufWinEnter", "nvim-dap")
+	load_on_event("BufWinEnter", "nvim-dap-ui")
+	load_on_event("BufWinEnter", "nvim-dap-virtual-text")
+	load_on_event("BufWinEnter", "DAPInstall.nvim", "dap-install")
+	vim.cmd([[
+	   augroup DapConfig
+	    autocmd!
+	    autocmd User JetpackDapInstall lua require("plugins-config.dap").config()
+	   augroup END
+	 ]])
 
 	-- list
-	vim.cmd([[autocmd! User spelunker.vim lua require("plugins-config.spelunker").config()]])
-	vim.cmd([[autocmd! User tabout.nvim lua require("plugins-config.tabout").config()]])
-	vim.cmd([[autocmd! User nvim-notify lua require("plugins-config.notify").config()]])
-	vim.cmd([[autocmd! User pretty-fold.nvim lua require("plugins-config.pretty-fold").config()]])
-	vim.cmd([[autocmd! User vim-sandwich lua require("plugins-config.sandwich").config()]])
-	vim.cmd([[autocmd! User substitute.nvim lua require("plugins-config.substitute").config()]])
-	vim.cmd([[autocmd! User kommentary lua require("plugins-config.kommentary").config()]])
-	vim.cmd([[autocmd! User nvim-colorizer.lua lua require("plugins-config.colorizer").config()]])
-	vim.cmd([[autocmd! User nvim-lightbulb lua require("plugins-config.lightbulb").config()]])
-	vim.cmd([[autocmd! User nvim-hlslens lua require("plugins-config.hlslens").config()]])
-	vim.cmd([[autocmd! User trouble.nvim lua require("plugins-config.trouble").config()]])
-	vim.cmd([[autocmd! User todo-comments.nvim lua require("plugins-config.todo-comments").config()]])
-	vim.cmd([[autocmd! User nvim-autopairs lua require("plugins-config.autopairs").config()]])
-	vim.cmd([[autocmd! User harpoon lua require("plugins-config.harpoon").config()]])
-	vim.cmd([[autocmd! User qf_helper.nvim lua require("plugins-config.qf_helper").config()]])
-	vim.cmd([[autocmd! User toggleterm.nvim lua require("plugins-config.toggleterm").config()]])
-	vim.cmd([[autocmd! User zen-mode.nvim lua require("plugins-config.zen-mode").config()]])
-	vim.cmd([[autocmd! User blamer.nvim lua require("plugins-config.blamer").config()]])
+	load_on_event("BufRead", "spelunker.vim", "spelunker")
+	require("plugins-config.spelunker").config()
+	require("plugins-config.tabout").config()
+	require("plugins-config.notify").config()
+	require("plugins-config.pretty-fold").config()
+	require("plugins-config.sandwich").config()
+	require("plugins-config.kommentary").config()
+	require("plugins-config.colorizer").config()
+	require("plugins-config.lightbulb").config()
+	require("plugins-config.hlslens").config()
+	require("plugins-config.trouble").config()
+	require("plugins-config.todo-comments").config()
+	require("plugins-config.autopairs").config()
+	require("plugins-config.harpoon").config()
+	require("plugins-config.qf_helper").config()
+	require("plugins-config.toggleterm").config()
+	require("plugins-config.zen-mode").config()
+	require("plugins-config.blamer").config()
 end
 load_config()
