95a1e6b364c2e16c49b507a515ccb20bc58a6440
annnenpolka <k.miyakewiss1@gmail.com>
2022-03-20 19:30:29 +0900

    set spelunker's highlight style to undercurl

--- lua/plugins-config/spelunker.lua
+++ lua/plugins-config/spelunker.lua
@@ -49,8 +49,8 @@ function M.config()
 
 	-- misspelled word highlighting
 	vim.cmd([[
-		highlight SpelunkerSpellBad cterm=underline ctermfg=247 gui=underline guifg=#9e9e9e
-		highlight SpelunkerComplexOrCompoundWord cterm=underline ctermfg=NONE gui=underline guifg=NONE
+		highlight SpelunkerSpellBad cterm=underline ctermfg=247 gui=undercurl guifg=#9e9e9e
+		highlight SpelunkerComplexOrCompoundWord cterm=underline ctermfg=NONE gui=undercurl guifg=NONE
 	]])
 	-- popup-menu highlight
 	vim.cmd([[
