c559ae78f2646a1f12ad483c1188ae1d0d511e6c
annnenpolka <k.miyakewiss1@gmail.com>
2022-03-26 16:08:09 +0900

    update mini-jump config to latest version

--- lua/plugins-config/mini-jump.lua
+++ lua/plugins-config/mini-jump.lua
@@ -13,9 +13,17 @@ function M.config()
 
 		-- Delay (in ms) between jump and highlighting all possible jumps. Set to
 		-- a very big number (like 10^7) to virtually disable highlighting.
-		highlight_delay = 50,
-		-- Timeout value(in ms) to stop jumping automatically.
-		idle_timeout = 1000,
+		-- highlight_delay = 500,
+
+		delay = {
+			-- Delay (in ms) between jump and highlighting all possible jumps. Set to
+			-- a very big number (like 10^7) to virtually disable highlighting.
+			highlight = 50,
+
+			-- Timeout value (in ms) to stop jumping automatically after idle. Set to
+			-- a very big number (like 10^7) to virtually disable timeout.
+			idle_stop = 1000,
+		},
 	})
 	-- define MiniJump highlight color
 	vim.cmd([[highlight link MiniJump Sneak]])
