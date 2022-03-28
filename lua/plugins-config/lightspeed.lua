dd6459c74dea450a28d34646dba1cf7a996093ca
annnenpolka <k.miyakewiss1@gmail.com>
2022-03-28 20:29:21 +0900

    add lightspeed visual mode keymap

--- lua/plugins-config/lightspeed.lua
+++ lua/plugins-config/lightspeed.lua
@@ -6,6 +6,10 @@ function M.map()
 	nmap("F", "<Plug>Lightspeed_F", "silent")
 	nmap("t", "<Plug>Lightspeed_t", "silent")
 	nmap("T", "<Plug>Lightspeed_T", "silent")
+	vmap("f", "<Plug>Lightspeed_f", "silent")
+	vmap("F", "<Plug>Lightspeed_F", "silent")
+	vmap("t", "<Plug>Lightspeed_t", "silent")
+	vmap("T", "<Plug>Lightspeed_T", "silent")
 end
 
 return M
