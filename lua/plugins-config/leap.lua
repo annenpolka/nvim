ee734f437bd53df48900bc0a869e5327049c043a
annnenpolka <k.miyakewiss1@gmail.com>
2022-03-28 18:04:24 +0900

    (leap.nvim) add resume searching keybind

--- lua/plugins-config/leap.lua
+++ lua/plugins-config/leap.lua
@@ -9,20 +9,56 @@ function M.config()
 		-- labels = { . . . },
 		-- These keys are captured directly by the plugin at runtime.
 		special_keys = {
-			next_match_group = "<space>",
-			prev_match_group = "<backspace>",
+			["next_match_group"] = "<space>",
+			["prev_match_group"] = "<tab>",
 			["repeat"] = "<enter>",
-			revert = "<Tab>",
-			accept_first_match = "L",
+			["revert"] = "<tab>",
 		},
 	})
-	vim.api.nvim_create_autocmd("User LeapEnter", { command = "nohlsearch" })
-	vim.api.nvim_create_autocmd("User LeapLeave", { command = "let &hlsearch=&hlsearch" })
 end
 
 function M.map()
 	nmap("J", "<Plug>(leap-forward)", "silent")
 	nmap("K", "<Plug>(leap-backward)", "silent")
+	local function set_leap_repeat_keys(repeat_key, revert_key)
+		local sk = require("leap").opts.special_keys
+
+		local function switch()
+			local temp = sk["repeat"]
+			sk["repeat"] = sk["revert"]
+			sk["revert"] = temp
+		end
+
+		sk["repeat"] = repeat_key
+		sk["revert"] = revert_key
+		vim.keymap.set({ "n", "x" }, repeat_key, ("<Plug>(leap-forward)" .. repeat_key))
+		vim.keymap.set({ "n", "x" }, revert_key, function()
+			vim.g.reverse_leap = true
+			return ("<Plug>(leap-backward)" .. revert_key)
+		end, { expr = true })
+
+		vim.api.nvim_create_augroup("LeapCustom", {})
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "LeapEnter",
+			group = "LeapCustom",
+			callback = function()
+				if vim.g.reverse_leap then
+					switch()
+				end
+			end,
+		})
+		vim.api.nvim_create_autocmd("User", {
+			pattern = "LeapLeave",
+			group = "LeapCustom",
+			callback = function()
+				if vim.g.reverse_leap then
+					switch()
+					vim.g.reverse_leap = nil
+				end
+			end,
+		})
+	end
+	set_leap_repeat_keys("<Enter>", "<S-Tab>")
 end
 
 return M
