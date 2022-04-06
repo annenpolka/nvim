d211f84f09f47f7e1a4cb45971506744f91387e9
annnenpolka <k.miyakewiss1@gmail.com>
2022-04-06 21:12:43 +0900

    add dap config loader at init, move configs to lua/dbg/languages

--- lua/dbg/init.lua
+++ lua/dbg/init.lua
@@ -1,2 +1,5 @@
-require("dbg.cpp")
-require("dbg.rust")
+-- require defined dap configuration for each language
+for _, ft_path in ipairs(vim.api.nvim_get_runtime_file("lua/dbg/languages/*.lua", true)) do
+	local ft = vim.fn.fnamemodify(ft_path, ":t:r")
+	require("dbg.languages." .. ft)
+end
