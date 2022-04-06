0ab1ffb2f4dff5210aae76b15fe8f0fb91745d54
Annenpolka <lancelbb@gmail.com>
2022-04-06 15:50:48 +0900

    add the noautocmd hack keymap for "@"(macro execution), formatting

--- lua/plugins-config/init.lua
+++ lua/plugins-config/init.lua
@@ -615,7 +615,7 @@ require("packer").startup({
 		})
 		-- treesitter-based context viewer
 		use({
-      disable = true,
+			disable = true,
 			"romgrk/nvim-treesitter-context",
 			requires = { "nvim-treesitter/nvim-treesitter" },
 			after = { "nvim-treesitter" },
