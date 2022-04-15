445ed162e565158f1a00a34e992fb8193ec8ff2c
Annenpolka <lancelbb@gmail.com>
2022-04-15 11:45:49 +0900

    chore: adjust blame delay from 300 to 500

--- lua/plugins-config/gitsigns.lua
+++ lua/plugins-config/gitsigns.lua
@@ -23,7 +23,7 @@ function M.config()
 		current_line_blame_opts = {
 			virt_text = true,
 			virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
-			delay = 300,
+			delay = 500,
 			ignore_whitespace = false,
 		},
 		current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
