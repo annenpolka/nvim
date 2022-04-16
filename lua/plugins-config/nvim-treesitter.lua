4bf6a5cb98352756b42ab977cb980853eaf03931
annnenpolka <k.miyakewiss1@gmail.com>
2022-04-16 21:55:31 +0900

    remove surround-funk's iF/aF mappings, replaced with treesitter @funcion

--- lua/plugins-config/nvim-treesitter.lua
+++ lua/plugins-config/nvim-treesitter.lua
@@ -26,8 +26,8 @@ function M.config()
 				lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
 				keymaps = {
 					-- You can use the capture groups defined in textobjects.scm
-					-- ["af"] = "@function.outer",
-					-- ["if"] = "@function.inner",
+					["aF"] = "@function.outer",
+					["iF"] = "@function.inner",
 					["ac"] = "@class.outer",
 					["ic"] = "@class.inner",
 					["ia"] = "@parameter.inner",
