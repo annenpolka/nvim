05b7dbc4e57d5d0c65136ebc3908dd3a2efb0a9f
annnenpolka <k.miyakewiss1@gmail.com>
2022-04-16 22:22:09 +0900

    add chainmember operator mapping to i./a.

--- lua/plugins-config/textobj-user.lua
+++ lua/plugins-config/textobj-user.lua
@@ -1,8 +1,16 @@
 local M = {}
 
 function M.map()
-	-- TODO: disable default keymaps for chainmember
+	vim.g.textobj_chainmember_no_default_key_mappings = 0
 	-- TODO: create keymaps for chainmember
+	xmap("im", "<Plug>(textobj-chainmember-i)", "silent")
+	omap("im", "<Plug>(textobj-chainmember-i)", "silent")
+	xmap("am", "<Plug>(textobj-chainmember-a)", "silent")
+	omap("am", "<Plug>(textobj-chainmember-a)", "silent")
+	xmap("i.", "<Plug>(textobj-chainmember-i)", "silent")
+	omap("i.", "<Plug>(textobj-chainmember-i)", "silent")
+	xmap("a.", "<Plug>(textobj-chainmember-a)", "silent")
+	omap("a.", "<Plug>(textobj-chainmember-a)", "silent")
 end
 
 return M
