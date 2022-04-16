fb4d6ccf68242f78462787a45941365616babda2
annnenpolka <k.miyakewiss1@gmail.com>
2022-04-16 16:00:51 +0900

    (copilot) add ignore filetype "registers"

--- lua/plugins-config/copilot.lua
+++ lua/plugins-config/copilot.lua
@@ -3,6 +3,9 @@ local M = {}
 function M.config()
 	vim.g.copilot_no_tab_map = true
 	vim.b.copilot_enabled = true
+	vim.g.copilot_filetypes = {
+		["registers"] = false,
+	}
 	imap("<C-J>", [[copilot#Accept('<CR>')]], { silent = true, script = true, expr = true })
 	imap("<C-z>", "<Plug>(copilot-next)")
 	imap("<C-x>", "<Plug>(copilot-previous)")
