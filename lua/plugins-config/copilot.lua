f0215f76c77c7c1cc89f747359dfd3de855d3d8c
annnenpolka <k.miyakewiss1@gmail.com>
2022-04-16 21:37:55 +0900

    unbind copilot completion, replaced with cmp-copilot

--- lua/plugins-config/copilot.lua
+++ lua/plugins-config/copilot.lua
@@ -6,7 +6,7 @@ function M.config()
 	vim.g.copilot_filetypes = {
 		["registers"] = false,
 	}
-	imap("<C-J>", [[copilot#Accept('<CR>')]], { silent = true, script = true, expr = true })
+	-- imap("<C-J>", [[copilot#Accept('<CR>')]], { silent = true, script = true, expr = true })
 	imap("<C-z>", "<Plug>(copilot-next)")
 	imap("<C-x>", "<Plug>(copilot-previous)")
 end
