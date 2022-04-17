ac29b750245cef6d7b741433f0020c5d310f6c28
annnenpolka <k.miyakewiss1@gmail.com>
2022-04-17 11:47:59 +0900

    disable sumneko_lua's neovim runtime file load

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -132,7 +132,7 @@ lsp_installer.on_server_ready(function(server)
 					},
 					workspace = {
 						-- Make the server aware of Neovim runtime files
-						library = vim.api.nvim_get_runtime_file("", true),
+						-- library = vim.api.nvim_get_runtime_file("", true),
 					},
 					-- Do not send telemetry data containing a randomized but unique identifier
 					telemetry = {
