6e302a22c03735e55593ea18a1ad5eed58134a69
Annenpolka <lancelbb@gmail.com>
2022-04-21 10:10:39 +0900

    update mini-surround config

--- lua/plugins-config/mini-surround.lua
+++ lua/plugins-config/mini-surround.lua
@@ -2,19 +2,16 @@ local M = {}
 
 function M.config()
 	require("mini.surround").setup({
-		-- Number of lines within which surrounding is searched
-		n_lines = 20,
+		-- Add custom surroundings to be used on top of builtin ones. For more
+		-- information with examples, see `:h MiniSurround.config`.
+		custom_surroundings = nil,
 
 		-- Duration (in ms) of highlight when calling `MiniSurround.highlight()`
 		highlight_duration = 500,
 
-		-- Pattern to match function name in 'function call' surrounding
-		-- By default it is a string of letters, '_' or '.'
-		funname_pattern = "[%w_%.]+",
-
 		-- Module mappings. Use `''` (empty string) to disable one.
 		mappings = {
-			add = "ys", -- Add surrounding
+			add = "sa", -- Add surrounding
 			delete = "sd", -- Delete surrounding
 			find = "sf", -- Find surrounding (to the right)
 			find_left = "sF", -- Find surrounding (to the left)
@@ -22,6 +19,14 @@ function M.config()
 			replace = "sr", -- Replace surrounding
 			update_n_lines = "sn", -- Update `n_lines`
 		},
+
+		-- Number of lines within which surrounding is searched
+		n_lines = 20,
+
+		-- How to search for surrounding (first inside current line, then inside
+		-- neighborhood). One of 'cover', 'cover_or_next', 'cover_or_prev',
+		-- 'cover_or_nearest'. For more details, see `:h MiniSurround.config`.
+		search_method = "cover_or_nearest",
 	})
 end
 
