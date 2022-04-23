feea723491c89ed1806b5488781c89e2e5913ec7
annnenpolka <k.miyakewiss1@gmail.com>
2022-04-23 20:37:15 +0900

    fix clippy config

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -88,8 +88,14 @@ lsp_installer.on_server_ready(function(server)
 		opts.settings = {
 			["rust-analyzer"] = {
 				checkOnSave = {
+					allFeatures = true,
 					overrideCommand = {
+						"cargo",
 						"clippy",
+						"--workspace",
+						"--message-format=json",
+						"--all-targets",
+						"--all-features",
 					},
 				},
 			},
