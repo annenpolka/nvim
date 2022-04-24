ef69bc959b016f973c468ae5c9467f31f3ac075b
annnenpolka <k.miyakewiss1@gmail.com>
2022-04-24 22:44:51 +0900

    set yanky.nvim telescope integration config

--- lua/plugins-config/yanky.lua
+++ lua/plugins-config/yanky.lua
@@ -1,26 +1,42 @@
 local M = {}
 
 function M.config()
+	local mapping = require("yanky.telescope.mapping")
 	require("yanky").setup({
-		{
-			ring = {
-				history_length = 10,
-				storage = "shada",
-				sync_with_numbered_registers = true,
-			},
-			system_clipboard = {
-				sync_with_ring = true,
-			},
-			highlight = {
-				on_put = true,
-				on_yank = true,
-				timer = 1000,
-			},
-			preserve_cursor_position = {
-				enabled = true,
+		ring = {
+			history_length = 10,
+			storage = "shada",
+			sync_with_numbered_registers = true,
+		},
+		system_clipboard = {
+			sync_with_ring = true,
+		},
+		highlight = {
+			on_put = true,
+			on_yank = true,
+			timer = 400,
+		},
+		preserve_cursor_position = {
+			enabled = true,
+		},
+		picker = {
+			telescope = {
+				mappings = {
+					default = mapping.put("p"),
+					i = {
+						-- ["<c-p>"] = mapping.put("p"),
+						-- ["<c-k>"] = mapping.put("P"),
+					},
+					n = {
+						["p"] = mapping.put("p"),
+						["P"] = mapping.put("P"),
+					},
+				},
 			},
 		},
 	})
+	-- integrate with telescope
+	require("telescope").load_extension("yank_history")
 end
 
 function M.map()
