2aa0370a213cd06692bfc9ff518031fc2669c35b
annnenpolka <k.miyakewiss1@gmail.com>
2022-04-26 11:39:37 +0900

    add exchange operator by substitute.nvim

--- lua/plugins-config/substitute.lua
+++ lua/plugins-config/substitute.lua
@@ -2,11 +2,16 @@ local M = {}
 
 function M.config()
 	require("substitute").setup()
-	-- operators
+	-- substitute operators
 	vim.keymap.set("n", "S", "<cmd>lua require('substitute').operator()<cr>", { noremap = true })
 	vim.keymap.set("n", "SS", "<cmd>lua require('substitute').line()<cr>", { noremap = true })
 	-- vim.keymap.set("n", "S", "<cmd>lua require('substitute').eol()<cr>", { noremap = true })
 	-- vim.keymap.set("x", "s", "<cmd>lua require('substitute').visual()<cr>", { noremap = true })
+	-- exchange operators
+	vim.keymap.set("n", "sx", "<cmd>lua require('substitute.exchange').operator()<cr>", { noremap = true })
+	vim.keymap.set("n", "sxx", "<cmd>lua require('substitute.exchange').line()<cr>", { noremap = true })
+	-- vim.keymap.set("x", "X", "<cmd>lua require('substitute.exchange').visual()<cr>", { noremap = true })
+	vim.keymap.set("n", "sxc", "<cmd>lua require('substitute.exchange').cancel()<cr>", { noremap = true })
 end
 
 return M
