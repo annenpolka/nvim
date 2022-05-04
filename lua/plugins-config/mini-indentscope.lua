7252308ccf788a2c4bc9a077462bea3178c4b8bc
annnenpolka <k.miyakewiss1@gmail.com>
2022-05-04 17:47:36 +0900

    rewrite mini-indentscope highlighting

--- lua/plugins-config/mini-indentscope.lua
+++ lua/plugins-config/mini-indentscope.lua
@@ -3,8 +3,9 @@ local M = {}
 function M.config()
 	require("mini.indentscope").setup({
 		draw = {
-			delay = 150,
-			animation = require("mini.indentscope").gen_animation("quadraticOut", { duration = 10, unit = "step" }),
+			delay = 100,
+			animation = require("mini.indentscope").gen_animation("quadraticOut", { duration = 5, unit = "step" }),
+			-- animation = require("mini.indentscope").gen_animation("none"),
 		},
 		-- Module mappings. Use `''` (empty string) to disable one.
 		mappings = {
@@ -21,11 +22,18 @@ function M.config()
 			indent_at_cursor = false,
 			try_as_border = true,
 		},
+		symbol = "╎",
+		-- symbol = "",
 	})
+
+	--- highlight
+	local h = function(...)
+		vim.api.nvim_set_hl(0, ...)
+	end
 	-- Sync highlight color to indent_blankline
-	vim.cmd([[highlight! link MiniIndentscopeSymbol CursorLine]])
+	h("MiniIndentscopeSymbol", { link = "IndentBlanklineContextChar" })
 	-- space highlight
-	vim.cmd([[highlight! link MiniIndentscopePrefix PMenuSel]])
+	-- h("MiniIndentscopePrefix", { link = "PMenuSel" })
 end
 
 return M
