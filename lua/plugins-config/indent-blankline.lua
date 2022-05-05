398dbd2b3c9a30eb072114290ac6e7a5b832067b
Annenpolka <lancelbb@gmail.com>
2022-05-05 10:51:04 +0900

    tweak indent-blankline config

--- lua/plugins-config/indent-blankline.lua
+++ lua/plugins-config/indent-blankline.lua
@@ -4,10 +4,10 @@ function M.config()
 	-- vim.g.indent_blankline_char = "┊"
 	vim.g.indent_blankline_filetype_exclude = { "help", "packer" }
 	vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
-	-- vim.g.indent_blankline_show_trailing_blankline_indent = true
+	-- vim.g.indent_blankline_show_trailing_blankline_indent = false
 
 	vim.cmd([[highlight IndentBlanklineContextStart guisp=#FA6A88 guibg=#181222 gui=underline]])
-	vim.cmd([[highlight IndentBlanklineContextChar guifg=#FE6A9E gui=nocombine]])
+	vim.cmd([[highlight IndentBlanklineContextChar guifg=#F0EAAE gui=nocombine]])
 	vim.cmd([[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]])
 	vim.cmd([[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]])
 	vim.cmd([[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]])
