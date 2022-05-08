a40d03cc3172106ec2adcabb4daed556cd1fa661
annnenpolka <k.miyakewiss1@gmail.com>
2022-05-08 16:38:22 +0900

    tweak highlight

--- lua/plugins-config/sherbet.lua
+++ lua/plugins-config/sherbet.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.config()
 	-- override highlights
 	vim.cmd("hi TabLine gui=NONE")
-	vim.cmd("hi CmpItemMenuDefault guifg=#555555")
+	vim.cmd("hi CmpItemMenuDefault guifg=#666666")
 end
 
 return M
