530b7247fc116419eca31754227178a7c10423e2
annnenpolka <k.miyakewiss1@gmail.com>
2022-05-08 14:11:43 +0900

    add cmp completion configs

--- lua/plugins-config/nvim-cmp.lua
+++ lua/plugins-config/nvim-cmp.lua
@@ -110,6 +110,11 @@ function M.config()
 				},
 			},
 		},
+		completion = {
+			-- completeopt = "menu,menuone,noinsert",
+			keyword_pattern = [[\%(-\?\d\+\%(\.\d\+\)\?\|\h\w*\%(-\w*\)*\)]],
+			keyword_length = 1,
+		},
 		formatting = {
 			-- devicons by lspkind
 			format = lspkind.cmp_format({
