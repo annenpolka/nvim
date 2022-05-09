2da293fb128dfc0d04dcbf49ac154bf178b98550
annnenpolka <k.miyakewiss1@gmail.com>
2022-05-09 22:13:55 +0900

    chore: comments

--- lua/plugins-config/dap.lua
+++ lua/plugins-config/dap.lua
@@ -26,6 +26,7 @@ function M.config()
 	-- ╰──────────────────────────────────────────────────────────╯
 	-- open quickfix automatically
 	vim.g.asyncrun_open = 6
+	-- task config file path
 	vim.g.asynctasks_extra_config = {
 		vim.fn.stdpath("config") .. "/tasks.ini",
 	}
