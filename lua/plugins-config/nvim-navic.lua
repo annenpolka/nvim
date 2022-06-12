9cebc613514067a2279e4de3452d101795329fad
Annenpolka <lancelbb@gmail.com>
2022-06-12 18:16:44 +0900

    refactor navic config, prepare for highlight settings

--- lua/plugins-config/nvim-navic.lua
+++ lua/plugins-config/nvim-navic.lua
@@ -2,40 +2,44 @@ local M = {}
 
 function M.config()
 	local navic = require("nvim-navic")
+	local icons = {
+		File = " ",
+		Module = " ",
+		Namespace = " ",
+		Package = " ",
+		Class = " ",
+		Method = " ",
+		Property = " ",
+		Field = " ",
+		Constructor = " ",
+		Enum = "練",
+		Interface = "練",
+		Function = " ",
+		Variable = " ",
+		Constant = " ",
+		String = " ",
+		Number = " ",
+		Boolean = "◩ ",
+		Array = " ",
+		Object = " ",
+		Key = " ",
+		Null = "ﳠ ",
+		EnumMember = " ",
+		Struct = " ",
+		Event = " ",
+		Operator = " ",
+		TypeParameter = " ",
+	}
 	navic.setup({
-		icons = {
-			File = " ",
-			Module = " ",
-			Namespace = " ",
-			Package = " ",
-			Class = " ",
-			Method = " ",
-			Property = " ",
-			Field = " ",
-			Constructor = " ",
-			Enum = "練",
-			Interface = "練",
-			Function = " ",
-			Variable = " ",
-			Constant = " ",
-			String = " ",
-			Number = " ",
-			Boolean = "◩ ",
-			Array = " ",
-			Object = " ",
-			Key = " ",
-			Null = "ﳠ ",
-			EnumMember = " ",
-			Struct = " ",
-			Event = " ",
-			Operator = " ",
-			TypeParameter = " ",
-		},
+		icons = icons,
 		highlight = false,
 		separator = " > ",
 		depth_limit = 0,
 		depth_limit_indicator = "..",
 	})
+	-- set_highlight("NavicText", { link = "SignColumn" })
+	-- set_highlight("NavicIconsFunction", { link = "AerialFunctionIcon" })
+	-- set_highlight("NavicIconsModule", { link = "FunctionModuleIcon" })
 end
 
 return M
