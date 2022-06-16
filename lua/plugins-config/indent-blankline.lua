5a70103a11dd1a52a76524954ca4e3e65d0f5afc
annnenpolka <k.miyakewiss1@gmail.com>
2022-06-16 22:35:15 +0900

    set indent-blankline char

--- lua/plugins-config/indent-blankline.lua
+++ lua/plugins-config/indent-blankline.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.config()
-	-- vim.g.indent_blankline_char = "┊"
+	vim.g.indent_blankline_char = "┊"
 	vim.g.indent_blankline_filetype_exclude = { "help", "packer" }
 	vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
 	-- vim.g.indent_blankline_show_trailing_blankline_indent = false
