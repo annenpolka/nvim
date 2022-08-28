a2f6d16d2c47889db1c83e4082ba9d26b85995c0
annenpolka <lancelbb@gmail.com>
2022-08-28 19:34:13 +0900

    set nvim-notify background_colour

--- lua/plugins-config/notify.lua
+++ lua/plugins-config/notify.lua
@@ -1,8 +1,11 @@
 local M = {}
 
 function M.config()
-    -- Replace notify with nvim-notify
-    vim.notify = require("notify")
+	-- Replace notify with nvim-notify
+	vim.notify = require("notify")
+	require("notify").setup({
+		background_colour = "#000000",
+	})
 end
 
 return M
