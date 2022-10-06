a428fdd47a638cb527a4f3430eeed7c231512756
annenpolka <lancelbb@gmail.com>
2022-10-06 21:59:30 +0900

    add notify configuration

--- lua/plugins-config/notify.lua
+++ lua/plugins-config/notify.lua
@@ -5,6 +5,20 @@ function M.config()
 	vim.notify = require("notify")
 	require("notify").setup({
 		background_colour = "#000000",
+		fps = 60,
+		icons = {
+			DEBUG = "",
+			ERROR = "",
+			INFO = "",
+			TRACE = "✎",
+			WARN = "",
+		},
+		level = 2,
+		minimum_width = 50,
+		render = "default",
+		stages = "fade_in_slide_out",
+		timeout = 3000,
+		top_down = true,
 	})
 end
 
