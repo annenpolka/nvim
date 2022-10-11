6b08c70a449205f663b0f63a31ae7df53f9020bd
annenpolka <lancelbb@gmail.com>
2022-10-11 14:50:45 +0900

    add leap multicursor config

--- lua/plugins-config/leap.lua
+++ lua/plugins-config/leap.lua
@@ -14,13 +14,57 @@ function M.config()
 		-- labels = { . . . },
 		special_keys = {
 			repeat_search = "<enter>",
-			next_match = "<enter>",
-			prev_match = "<tab>",
+			next_aot_match = "<enter>",
+			next_match = { ";", "<enter>" },
+			prev_match = { ",", "<tab>" },
 			next_group = "<space>",
 			prev_group = "<tab>",
 		},
 	})
 	-- require("leap").set_default_keymaps()
+end
+
+function M.multi_normal()
+	-- The following example showcases a custom action, using `multiselect`. We're
+	-- executing a `normal!` command at each selected position (this could be even
+	-- more useful if we'd pass in custom targets too).
+
+	local function paranormal(targets)
+		-- Get the :normal sequence to be executed.
+		local input = vim.fn.input("normal! ")
+		if #input < 1 then
+			return
+		end
+
+		local ns = vim.api.nvim_create_namespace("")
+
+		-- Set an extmark as an anchor for each target, so that we can also execute
+		-- commands that modify the positions of other targets (insert/change/delete).
+		for _, target in ipairs(targets) do
+			local line, col = unpack(target.pos)
+			local id = vim.api.nvim_buf_set_extmark(0, ns, line - 1, col - 1, {})
+			target.extmark_id = id
+		end
+
+		-- Jump to each extmark (anchored to the "moving" targets), and execute the
+		-- command sequence.
+		for _, target in ipairs(targets) do
+			local id = target.extmark_id
+			local pos = vim.api.nvim_buf_get_extmark_by_id(0, ns, id, {})
+			vim.fn.cursor(pos[1] + 1, pos[2] + 1)
+			vim.cmd("normal! " .. input)
+		end
+
+		-- Clean up the extmarks.
+		vim.api.nvim_buf_clear_namespace(0, ns, 0, -1)
+	end
+
+	-- Usage:
+	require("leap").leap({
+		target_windows = { vim.fn.win_getid() },
+		action = paranormal,
+		multiselect = true,
+	})
 end
 
 function M.map()
@@ -29,6 +73,7 @@ function M.map()
 	vim.keymap.set({ "n", "v", "o" }, "S", function()
 		require("leap").leap({ target_windows = { vim.fn.win_getid() } })
 	end, { silent = true })
+	-- vim.keymap.set({ "n", "v", "o" }, "<C-n>", M.multi_normal, { silent = true })
 end
 
 return M
