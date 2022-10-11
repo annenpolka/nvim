c09ce8174e54ef081d90e94643d7b2aa593b1d69
annenpolka <lancelbb@gmail.com>
2022-10-11 14:51:29 +0900

    tweak windows.nvim config

--- lua/plugins-config/windows.lua
+++ lua/plugins-config/windows.lua
@@ -7,22 +7,35 @@ function M.config()
 	require("windows").setup({
 		autowidth = { --		     |windows.autowidth|
 			enable = true,
-			winwidth = 5, --		      |windows.winwidth|
+			winwidth = 10, --		      |windows.winwidth|
 			filetype = { --	    |windows.autowidth.filetype|
 				help = 2,
 			},
 		},
 		ignore = { --			|windows.ignore|
-			buftype = { "quickfix" },
-			filetype = { "NvimTree", "neo-tree", "undotree", "gundo", "ddu-ff", "ddu-ff-filter", "harpoon" },
+			buftype = { "quickfix", "nofile" },
+			filetype = {
+				"NvimTree",
+				"neo-tree",
+				"undotree",
+				"gundo",
+				"ddu-ff",
+				"ddu-ff-filter",
+				"harpoon",
+				"TelescopePrompt",
+			},
 		},
 		animation = {
 			enable = true,
-			duration = 200,
-			fps = 30,
+			duration = 150,
+			fps = 60,
 			easing = "in_out_sine",
 		},
 	})
+end
+
+function M.map()
+	vim.keymap.set("n", "<CR>", "<Cmd>WindowsMaximize<CR>")
 end
 
 return M
