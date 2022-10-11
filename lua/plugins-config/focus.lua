cc90fde95d0429ebaa9a92ef57fdfe0b56b752c4
annenpolka <lancelbb@gmail.com>
2022-10-11 17:07:06 +0900

    tweak focus config

--- lua/plugins-config/focus.lua
+++ lua/plugins-config/focus.lua
@@ -4,12 +4,20 @@ function M.config()
 	local focus = require("focus")
 	-- vim.wo.signcolumn = "number" -- set before focus loaded
 	focus.setup({
-		autoresize = false,
-		excluded_filetypes = { "toggleterm", "harpoon", "ctrlspace", "TelescopePrompt", "ddu-ff", "ddu-ff-filter" },
+		autoresize = true,
+		excluded_filetypes = {
+			"toggleterm",
+			"harpoon",
+			"ctrlspace",
+			"TelescopePrompt",
+			"ddu-ff",
+			"ddu-ff-filter",
+			"OverseerList",
+		},
 		excluded_buftypes = { "acwrite", "prompt", "nofile", "terminal" },
 		winhighlight = true,
 		hybridnumber = false,
-		signcolumn = "number", -- set before focus loaded
+		signcolumn = "number",
 	})
 	vim.cmd([[
     hi link UnfocusedWindow CursorLine
@@ -18,12 +26,12 @@ function M.config()
 end
 
 function M.map()
-	nnoremap("<leader>wh", ":FocusSplitLeft<CR>", { silent = true })
-	nnoremap("<leader>wj", ":FocusSplitDown<CR>", { silent = true })
-	nnoremap("<leader>wk", ":FocusSplitUp<CR>", { silent = true })
-	nnoremap("<leader>wl", ":FocusSplitRight<CR>", { silent = true })
-	nnoremap("<CR>", ":FocusMaxOrEqual<CR>", { silent = true })
-	nnoremap("<CR>", "<CR>", { ft = "vim" }) -- avoid overding vim's <CR> in commandline mode
+	vim.keymap.set("n", "<leader>wh", ":FocusSplitLeft<CR>", { silent = true })
+	vim.keymap.set("n", "<leader>wj", ":FocusSplitDown<CR>", { silent = true })
+	vim.keymap.set("n", "<leader>wk", ":FocusSplitUp<CR>", { silent = true })
+	vim.keymap.set("n", "<leader>wl", ":FocusSplitRight<CR>", { silent = true })
+	vim.keymap.set("n", "<CR>", ":FocusMaxOrEqual<CR>", { silent = true })
+	vim.keymap.set("n", "<CR>", "<CR>", { ft = "vim" }) -- avoid overding vim's <CR> in commandline mode
 end
 
 return M
