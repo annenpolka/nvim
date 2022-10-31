ce594d19657aeaa1be619b817c64470d351d6303
annenpolka <lancelbb@gmail.com>
2022-10-31 17:05:51 +0900

    unbind scrollbar color

--- lua/plugins-config/scrollbar.lua
+++ lua/plugins-config/scrollbar.lua
@@ -1,22 +1,23 @@
 local M = {}
 
 function M.config()
+	require("scrollbar").setup()
+
 	-- use tokyonight colorscheme
 	local colors = require("tokyonight.colors").setup()
-
-	require("scrollbar").setup({
-		handle = {
-			color = colors.bg_highlight,
-		},
-		marks = {
-			Search = { color = colors.orange },
-			Error = { color = colors.error },
-			Warn = { color = colors.warning },
-			Info = { color = colors.info },
-			Hint = { color = colors.hint },
-			Misc = { color = colors.purple },
-		},
-	})
+	-- require("scrollbar").setup({
+	-- 	handle = {
+	-- 		color = colors.bg_highlight,
+	-- 	},
+	-- 	marks = {
+	-- 		Search = { color = colors.orange },
+	-- 		Error = { color = colors.error },
+	-- 		Warn = { color = colors.warning },
+	-- 		Info = { color = colors.info },
+	-- 		Hint = { color = colors.hint },
+	-- 		Misc = { color = colors.purple },
+	-- 	},
+	-- })
 
 	require("scrollbar.handlers.search").setup()
 end
