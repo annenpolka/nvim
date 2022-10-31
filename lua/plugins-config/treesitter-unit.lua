0b4d70a9ed3e858e55c61e0f0a3d7be87f8cd533
annenpolka <lancelbb@gmail.com>
2022-10-31 11:56:36 +0900

    tweak unit keymap

--- lua/plugins-config/treesitter-unit.lua
+++ lua/plugins-config/treesitter-unit.lua
@@ -18,10 +18,10 @@ function M.config()
 	set_highlight("TSUnit", { bg = "#35354A" })
 
 	-- text objects
-	vim.keymap.set("x", "iu", ':lua require"treesitter-unit".select()<CR>', { noremap = true })
-	vim.keymap.set("x", "au", ':lua require"treesitter-unit".select(true)<CR>', { noremap = true })
-	vim.keymap.set("o", "iu", ':<c-u>lua require"treesitter-unit".select()<CR>', { noremap = true })
-	vim.keymap.set("o", "au", ':<c-u>lua require"treesitter-unit".select(true)<CR>', { noremap = true })
+	vim.keymap.set("x", "iu", ':lua require"treesitter-unit".select(true)<CR>', { noremap = true })
+	vim.keymap.set("x", "au", ':lua require"treesitter-unit".select()<CR>', { noremap = true })
+	vim.keymap.set("o", "iu", ':<c-u>lua require"treesitter-unit".select(true)<CR>', { noremap = true })
+	vim.keymap.set("o", "au", ':<c-u>lua require"treesitter-unit".select()<CR>', { noremap = true })
 end
 
 return M
