6854c97aa3265809f3b254ee2530f48241b5e21a
Annenpolka <lancelbb@gmail.com>
2022-12-27 22:32:40 +0900

    (flit) disable labeled_modes to avoid errors

--- lua/plugins-config/flit.lua
+++ lua/plugins-config/flit.lua
@@ -3,7 +3,7 @@ function M.config()
 	require("flit").setup({
 		keys = { f = "f", F = "F", t = "t", T = "T" },
 		-- A string like "nv", "nvo", "o", etc.
-		labeled_modes = "v",
+		labeled_modes = "",
 		multiline = true,
 		-- Like `leap`s similar argument (call-specific overrides).
 		-- E.g.: opts = { equivalence_classes = {} }
